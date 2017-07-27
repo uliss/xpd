efc56b4214aff31773b15e7c926173f9bb32313e
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-28 01:16:39 +0300

    atom 100% coverage

---
+++ tests/test_cpd_atom.cpp
@@ -0,0 +1,34 @@
+#include "catch.hpp"
+
+#include "atom.h"
+#include "lib_cpd.h"
+
+#include <string>
+
+static int init = cpd_init();
+
+TEST_CASE("cpd_atom", "[cpd PureData wrapper]")
+{
+    SECTION("init")
+    {
+        auto a0 = cpd_atom_float_new(123);
+        REQUIRE(cpd_atom_float(a0) == 123);
+        REQUIRE(cpd_atom_is_float(a0));
+        REQUIRE_FALSE(cpd_atom_is_symbol(a0));
+        REQUIRE(cpd_atom_symbol(a0) == 0);
+
+        cpd_atom_set_float(a0, -200);
+        REQUIRE(cpd_atom_float(a0) == -200);
+
+        cpd_atom_set_symbol(a0, "ABC");
+        REQUIRE(cpd_atom_float(a0) == 0);
+        REQUIRE(cpd_atom_symbol(a0) == std::string("ABC"));
+
+        cpd_atom_free(a0);
+
+        auto a1 = cpd_atom_symbol_new("XYZ");
+        REQUIRE(cpd_atom_is_symbol(a1));
+        REQUIRE(cpd_atom_symbol(a1) == std::string("XYZ"));
+        cpd_atom_free(a1);
+    }
+}
