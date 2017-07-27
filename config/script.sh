3e373b312fc4f2712cba0e1f4890014369fbceee
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-27 15:49:34 +0300

    travis setup

---
+++ config/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir build && cd build
+if [ -z "$COVERALLS" ]
+then
+    cmake -DCOVERALLS=On -DCMAKE_BUILD_TYPE=Debug ..
+else
+    cmake -DCMAKE_BUILD_TYPE=Debug ..
+fi
+
+cmake --build .
+
+make test
+
