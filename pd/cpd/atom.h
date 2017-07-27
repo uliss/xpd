d6daa26adee58729e54991df160b0e4fa99f9694
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-28 01:22:39 +0300

    doc added

--- pd/cpd/atom.h
+++ pd/cpd/atom.h
@@ -8,14 +8,66 @@ extern "C" {
 struct _atom;
 typedef struct _atom t_cpd_atom;
 
+/**
+ * Create float atom
+ * @param f - atom value
+ * @returns pointer to new atom, it should be free via cpd_atom_free 
+ */
 t_cpd_atom* cpd_atom_float_new(float f);
+
+/**
+ * Create symbol atom
+ * @param s - string value, that is converted to symbol via gensym() call
+ * @return  pointer to new atom
+ */
 t_cpd_atom* cpd_atom_symbol_new(const char* s);
+
+/**
+ * Sets atom float value and set it's type to float
+ * @param a - pointer to atom
+ * @param f - float value
+ */
 void cpd_atom_set_float(t_cpd_atom* a, float f);
+
+/**
+ * Sets atom symbol value and set it's type to symbol
+ * @param a - pointer to atom
+ * @param s - string value
+ */
 void cpd_atom_set_symbol(t_cpd_atom* a, const char* s);
+
+/**
+ * Checks if float atom
+ * @param a - pointer to atom
+ * @return 1 if float, otherwise 0
+ */
 int cpd_atom_is_float(t_cpd_atom* a);
+
+/**
+ * Checks if symbol atom
+ * @param a - pointer to atom
+ * @return 1 if symbol, otherwise 0
+ */
 int cpd_atom_is_symbol(t_cpd_atom* a);
+
+/**
+ * Returns float atom value
+ * @param a - pointer to atom
+ * @returns float value or 0 if not float, or NULL
+ */
 float cpd_atom_float(t_cpd_atom* a);
+
+/**
+ * Returns symbol atom value
+ * @param a - pointer to atom
+ * @return C-string value or NULL if not symbol or NULL
+ */
 const char* cpd_atom_symbol(t_cpd_atom* a);
+
+/**
+ * Destruct atom, created via cpd_atom_float etc.
+ * @param a - pointer to atom
+ */
 void cpd_atom_free(t_cpd_atom* a);
 
 #ifdef __cplusplus
