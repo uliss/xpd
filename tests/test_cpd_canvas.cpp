c0e797a21ead0958088347d5fa17bbc790003b10
Serge Poltavski <serge.poltavski@gmail.com>
2017-07-28 16:37:43 +0300

    canvas tests++

--- tests/test_cpd_canvas.cpp
+++ tests/test_cpd_canvas.cpp
@@ -16,6 +16,7 @@ TEST_CASE("cpd_canvas", "[cpd PureData wrapper]")
 
         auto c0 = cpd_root_canvas_new();
         REQUIRE(c0);
+        REQUIRE(cpd_canvas_root(c0) == c0);
         REQUIRE(cpd_root_canvas_count() == 4);
         REQUIRE(cpd_root_canvas_at(0) == c0);
         REQUIRE(cpd_canvas_current() == c0);
@@ -51,5 +52,36 @@ TEST_CASE("cpd_canvas", "[cpd PureData wrapper]")
         REQUIRE(cpd_canvas_name(cpd_root_canvas_at(0)) == std::string("_float_array_template"));
         REQUIRE(cpd_canvas_name(cpd_root_canvas_at(1)) == std::string("_float_template"));
         REQUIRE(cpd_canvas_name(cpd_root_canvas_at(2)) == std::string("_text_template"));
+
+        REQUIRE(cpd_canvas_name(nullptr) == nullptr);
+    }
+
+    SECTION("root")
+    {
+        REQUIRE(cpd_canvas_root(nullptr) == nullptr);
+        REQUIRE_FALSE(cpd_canvas_is_root(nullptr));
+        auto c0 = cpd_root_canvas_new();
+        REQUIRE(cpd_canvas_is_root(c0));
+        REQUIRE(cpd_canvas_root(c0) == c0);
+        cpd_canvas_free(c0);
+    }
+
+    SECTION("canvas directory")
+    {
+        REQUIRE(cpd_root_canvas_dir(nullptr) == std::string());
+        auto c0 = cpd_root_canvas_new();
+        REQUIRE(cpd_root_canvas_dir(c0) == std::string("~/"));
+        cpd_canvas_free(c0);
+    }
+
+    SECTION("canvas coords")
+    {
+        SECTION("root")
+        {
+            REQUIRE(cpd_root_canvas_x(0) == 0);
+            REQUIRE(cpd_root_canvas_y(0) == 0);
+            REQUIRE(cpd_root_canvas_width(0) == 0);
+            REQUIRE(cpd_root_canvas_height(0) == 0);
+        }
     }
 }
